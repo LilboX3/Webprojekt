30b5d27152ace74229759fcf7ad5cff15d7db564
12032554 <W1a2p3p4l5e6r7>
2023-01-15 15:40:28 +0100

    improved homepage (zindex in navbar)

--- pages/Homepage.php
+++ pages/Homepage.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="StyleNav.css">
+    <link rel="stylesheet" href="Dropdown.css">
     <title>Home</title>
     <style>
         .background {
@@ -44,22 +45,27 @@
     <div class="background">
         <div class="row">
             <div class="col-2"></div>
-            <div class="col-8 box" style="margin-top:5%;text-align:center;">
-                <h2>Erleben Sie Des Nâtes</h2>
+            <div class="col-8" style="margin-top:5%;color:white;">
+                <h1>Erleben Sie Des Nâtes</h1>
                 <p>"Ein einmaliges Erlebnis!" - 10/10 Top 10 Goofiest Hotels Daily</p>
-                <p style="font-size:smaller;">"Es ist einfach eins der Hotels jemals" - Gast</p>
+                <p style="font-size:smaller;">"Es ist einfach eines der Hotels jemals" - Gast</p>
             </div>
             <div class="col-2"></div>
         </div>
         <div class="row">
-        <div class="col-4"></div>
-            <div class="col-4 box2" style="margin-top:1%; text-align:center;">
-                <p><a href="Registrierung.php">Erstellen Sie ein Konto</a></p>
-                <p><a href="News.php">Hotel-News</a></p>
-                <p><a href="Galerie.php">Bilder</a></p>
-                <p><a href="FAQ.php">Mehr erfahren</a></p>
+        <div class="col-2"></div>
+            <div class="col-8" style="margin-top:1%;">
+            <div class="dropdown">
+            <button class="dropbtn" style="width:150%;">Mehr erfahren</button>
+            <div class="dropdown-content">
+                <a href="Registrierung.php">Account erstellen</a>
+                <a href="News.php">Hotel News</a>
+                <a href="Galerie.php">Bilder</a>
+                <a href="FAQ.php">Mehr Information</a>
             </div>
-            <div class="col-4"></div>
+            </div>
+            </div>
+            <div class="col-2"></div>
         </div>
     </div>
     
